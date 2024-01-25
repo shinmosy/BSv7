058047da711e81dae94f5821d352dee605fc9788
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-01-25 18:59:48 +0800

    Add files via upload

--- plugins/Auto-function/auto-action.js
+++ plugins/Auto-function/auto-action.js
@@ -1,25 +1,36 @@
-import {
-    executeActions
-} from '../../main.js';
+import { executeActions } from '../../main.js';
 import ora from 'ora';
 import chalk from 'chalk';
 
 export async function before(m) {
-    const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+    let lastActionTime = new Date().getTime();
 
-    setInterval(async () => {
+    const executeAndLog = async () => {
         try {
+            const currentTime = new Date().getTime();
+            const elapsedMinutes = (currentTime - lastActionTime) / (60 * 1000);
+
+            if (elapsedMinutes >= 15) {
+                const loadingSpinner = ora({
+                    text: chalk.cyan('Menjalankan tugas berkala...'),
+                    spinner: 'moon'
+                }).start();
+
+                await executeActions();
+                loadingSpinner.render();
+                loadingSpinner.succeed(chalk.green('Tugas berkala selesai.'));
+
+                lastActionTime = currentTime;
+            }
+        } catch (error) {
             const loadingSpinner = ora({
-                text: chalk.cyan('Menjalankan tugas berkala...'),
-                spinner: 'moon'
+                text: chalk.red(`Gagal menjalankan tugas berkala: ${error.message}`),
+                spinner: 'fail'
             }).start();
 
-            await executeActions();
-            loadingSpinner.render();
-            loadingSpinner.succeed(chalk.green('Tugas berkala selesai.'));
-        } catch (error) {
-        loadingSpinner.fail(chalk.red(`Gagal menjalankan tugas berkala: ${error.message}`));
             console.error(chalk.red(`Error during periodic execution: ${error.message}`));
+            loadingSpinner.stop();
         }
-    }, 15 * 60 * 1000);
+    };
+    setInterval(executeAndLog, 15 * 60 * 1000);
 }
