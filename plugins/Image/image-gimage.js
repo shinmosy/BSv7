5e16d54d31bc4d42669c5ea88f9a3a00109e6f16
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-02-04 13:37:33 +0800

    Add files via upload

--- plugins/Image/image-gimage.js
+++ plugins/Image/image-gimage.js
@@ -1,99 +1,65 @@
-import fetch from "node-fetch"
-import axios from "axios"
-import cheerio from "cheerio"
-import {
-    googleImage
-} from "@bochilteam/scraper"
-import {
-    readFileSync
-} from "fs"
-import got from "got"
+import fetch from 'node-fetch';
+import { googleImage } from '@bochilteam/scraper';
+import cheerio from 'cheerio';
 
-let handler = async (m, {
-    conn,
-    args,
-    usedPrefix,
-    text,
-    command
-}) => {
+const handler = async (m, { text, usedPrefix, command }) => {
+    const lister = ["v1", "v2", "v3"];
+    let [inputs, feature] = text.split("|");
+    const msg = `Masukkan input yang valid\n\n*Contoh:*\n${usedPrefix + command} link|v2\n\n*Pilih versi yang ada:*\n${lister
+        .map((v) => `  ○ ${v.toUpperCase()}`)
+        .join("\n")}`;
+    
+    let res;
+    
+    feature = feature || "v1";
+    if (!lister.includes(feature.toLowerCase())) return m.reply(msg);
 
-    let lister = [
-        "v1",
-        "v2",
-        "v3",
-        "v4"
-    ]
+    try {
+        switch (feature) {
+            case "v1":
+                if (!inputs) return m.reply("Input query link\nExample: .image jokowi|v2");
+                await m.reply(wait);
+                res = await googleImage(inputs);
+                break;
 
-    let [feature, inputs, inputs_, inputs__, inputs___] = text.split("|")
-    if (!lister.includes(feature.toLowerCase())) return m.reply("*Example:*\n.image search|vpn\n\n*Pilih type yg ada*\n" + lister.map((v, index) => "  ○ " + v.toUpperCase()).join("\n"))
+            case "v2":
+                if (!inputs) return m.reply("Input query link\nExample: .image jokowi|v2");
+                await m.reply(wait);
+                res = await GoogleImage(inputs);
+                break;
 
-    if (lister.includes(feature.toLowerCase())) {
-
-        if (feature == "v1") {
-            if (!inputs) return m.reply("Input query link\nExample: .image V1|jokowi")
-            await m.reply(wait)
-            try {
-                let res = await GoogleImage(inputs)
-                let teks = "🔍 *[ RESULT ]*"
-                await conn.sendFile(m.chat, res.getRandom() || logo, "", teks, m)
-            } catch (e) {
-                await m.reply(eror)
-            }
+            case "v3":
+                res = await (await fetch(`https://api.lolhuman.xyz/api/gimage2?apikey=${lolkey}&query=${inputs}`, {
+                    headers: {
+                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+                        'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
+                    },
+                })).json();
+                break;
         }
 
-        if (feature == "v2") {
-            if (!inputs) return m.reply("Input query link\nExample: .image V1|jokowi")
-            await m.reply(wait)
-            try {
-                let res = await googleImage(inputs)
-                let teks = "🔍 *[ RESULT ]*"
-                await conn.sendFile(m.chat, res.getRandom() || logo, "", teks, m)
-            } catch (e) {
-                await m.reply(eror)
-            }
-        }
-
-        if (feature == "v3") {
-            if (!inputs) return m.reply("Input query link\nExample: .image V1|jokowi")
-            await m.reply(wait)
-            try {
-
-            } catch (e) {
-                await m.reply(eror)
-            }
-        }
-
-        if (feature == "v4") {
-            if (!inputs) return m.reply("Input query link\nExample: .image V1|jokowi")
-            await m.reply(wait)
-            try {
-                let res = await (await fetch(`https://api.lolhuman.xyz/api/gimage2?apikey=${lolkey}&query=${inputs}`)).json()
-                let teks = "🔍 *[ RESULT ]*"
-                await conn.sendFile(m.chat, ((res.result).getRandom()) || logo, "", teks, m)
-            } catch (e) {
-                await m.reply(eror)
-            }
-        }
-
-
-
+        const teks = `🔍 *[ RESULT ${feature}]*`;
+        await conn.sendFile(m.chat, res.getRandom() || logo, "", teks, m);
+    } catch (e) {
+        await m.reply(eror);
     }
-}
-handler.help = ["image"]
-handler.tags = ["internet"]
-handler.command = /^(g?image)$/i
-export default handler
+};
+
+handler.help = ["image"];
+handler.tags = ["internet"];
+handler.command = /^(g?image)$/i;
+export default handler;
 
 /* New Line */
 async function GoogleImage(query) {
-    const data = await got(`https://www.google.com/search?q=${query}&tbm=isch`, {
+    const response = await fetch(`https://www.google.com/search?q=${query}&tbm=isch`, {
         headers: {
-            accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-            "accept-encoding": "gzip, deflate, br",
-            "accept-language": "en-US,en;q=0.9,id;q=0.8",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9,id;q=0.8',
         },
-    }).text();
+    });
+
+    const data = await response.text();
 
     const $ = cheerio.load(data);
     const pattern =
@@ -101,8 +67,6 @@ async function GoogleImage(query) {
     const matches = $.html().matchAll(pattern);
     const decodeUrl = (url) => decodeURIComponent(JSON.parse(`"${url}"`));
     return [...matches]
-        .map(({
-            groups
-        }) => decodeUrl(groups?.url))
+        .map(({ groups }) => decodeUrl(groups?.url))
         .filter((v) => /.*\.jpe?g|png$/gi.test(v));
 }
