35c304409e059d95d4238885e134b77798cf386c
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-02-07 20:20:56 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,6 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 import {
     loadConfig
 } from './config.js';
-import {
-    runSyntaxCheck
-} from './test.js';
 
 import {
     createRequire
--- test.js
+++ test.js
@@ -141,5 +141,4 @@ const formatResult = (result) => {
     ].join(' ');
 };
 
-export { runSyntaxCheck };
 runSyntaxCheck();
