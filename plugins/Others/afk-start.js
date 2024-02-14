2c85777dc2e6b34cfa678107d12a7d9b9505bec2
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-02-14 22:11:33 +0800

    Add files via upload

--- plugins/Others/afk-start.js
+++ plugins/Others/afk-start.js
@@ -4,10 +4,19 @@ let handler = async (m, {
     usedPrefix,
     command
 }) => {
+conn.listAfk = conn.listAfk || {};
     try {
         let user = global.db.data.users[m.sender]
         user.afk = +new Date();
         user.afkReason = text;
+        const username = m.name || m.pushName;
+        const id = m.sender || m.key.remoteJid;
+
+conn.listAfk[m.chat] = conn.listAfk[m.chat]
+  ? conn.listAfk[m.chat].some(user => user.id === id)
+    ? conn.listAfk[m.chat]
+    : [...conn.listAfk[m.chat], { username, id }]
+  : [{ username, id }];
 
         const caption = `\n🚀 ${await conn.getName(m.sender)} @${m.sender.split("@")[0]} Sekarang lagi AFK\n*Dengan Alasan:*\n${text ? ' ' + text : 'Tanpa alasan'}`;
 
