8dd572fbb3f12da73d0f6b7c52edf6539d6a8621
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-02-17 17:58:15 +0800

    Add files via upload

--- plugins/Auto-function/auto-didyoumean.js
+++ plugins/Auto-function/auto-didyoumean.js
@@ -8,43 +8,36 @@ export async function before(m, { match, usedPrefix }) {
   try {
     if ((usedPrefix = (match[0] || '')[0])) {
       const noPrefix = m.text.replace(usedPrefix, '').trim().toLowerCase();
-
       const helpPromises = Object.values(plugins)
         .filter(v => v.help && !v.disabled)
         .map(async v => v.help.map(entry => entry.trim().split(' ')[0].toLowerCase()))
-
       const help = await Promise.all(helpPromises).then(arrays => arrays.flat());
-
       const { winner, matched } = didyoumean3(noPrefix, help);
-
+      if (winner && matched && noPrefix) {
       if (winner !== noPrefix) {
-        const filteredMatches = matched.filter(item => item.score < 4);
-
+        const filteredMatches = matched.filter(item => item.score < 3);
         const groupedByScore = filteredMatches.reduce((acc, item) => {
           const score = 10 - item.score;
           const target = item.target.trim();
           const scoreKey = Math.max(1, Math.min(10, score));
-
           acc[scoreKey] = acc[scoreKey] ? [...acc[scoreKey], target] : [target];
           return acc;
         }, {});
-
         const resultText = Object.entries(groupedByScore)
   .sort(([scoreA], [scoreB]) => scoreB - scoreA)
-  .slice(0, 4)
+  .filter(item => item.target !== null && item.target !== undefined && item.target !== '' && item.target !== ' ')
+  .slice(0, 3)
   .filter(([score, suggestions]) => score.trim() && suggestions.length > 0)
   .map(([score, suggestions], index) => {
     const adjustedScore = Math.max(1, Math.min(10, 10 - parseInt(score)));
     const scoreString = `${adjustedScore}`;
-    const formattedSuggestions = suggestions.slice(0, 4).map(suggestion => `   - ${usedPrefix + suggestion}`).join('\n');
+    const formattedSuggestions = suggestions.slice(0, 3).map(suggestion => `   - ${usedPrefix + suggestion}`).join('\n');
     return `*${index + 1}.* Similarity: *${parseInt(Number(10 - scoreString))}/10*\n${formattedSuggestions}`;
   })
   .join('\n\n');
-
         if (resultText) {
           const mentionedJid = m.mentionedJid?.[0] ?? (m.fromMe ? this.user.jid : m.sender);
           const senderName = (await this.getName(mentionedJid)).split('\n')[0];
-
           await this.sendMessage(m.chat, {
             text: `👋 Hai ${senderName} @${mentionedJid.split('@')[0]} !\n*Apakah maksudmu:* ${getRandomEmoji()}\n${resultText}`,
             mentions: [mentionedJid]
@@ -53,6 +46,7 @@ export async function before(m, { match, usedPrefix }) {
           });
         }
       }
+    }
     }
   } catch (error) {
     console.error("Terjadi kesalahan:", error);
