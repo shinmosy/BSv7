818fbb262c59edebaa68352e6b8311a6255d2d99
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-03-06 13:10:20 +0800

    Add files via upload

--- plugins/Game-answer/game-akinator-ans.js
+++ plugins/Game-answer/game-akinator-ans.js
@@ -1,64 +1,57 @@
-import fetch from 'node-fetch'
 const teks = '0 - Ya\n1 - Tidak\n2 - Saya Tidak Tau\n3 - Mungkin\n4 - Mungkin Tidak\n5 - Kembali ke pertanyaan sebelumnya'
 
 export async function before(m) {
     if (global.db.data.users[m.sender].banned) return
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text) return !0
-    let aki = global.db.data.users[m.sender].akinator
-    if (!aki.sesi || m.quoted.id != aki.soal.key.id) return
-    if (!somematch(['0', '1', '2', '3', '4', '5'], m.text)) return this.sendMessage(m.chat, {
-        text: `[!] Jawab dengan angka 1, 2, 3, 4, atau 5\n\n${teks}`
-    }, {
-        quoted: aki.soal
-    })
-    let {
-        server,
-        frontaddr,
-        session,
-        signature,
-        question,
-        progression,
-        step
-    } = aki
-    if (step == '0' && m.text == '5') return m.reply('Anda telah mencapai pertanyaan pertama')
-    let res, anu, soal
+    this.akinator = this.akinator || {};
+    if (!this.akinator[m.sender] || m.quoted.id != this.akinator[m.sender].msg.key.id) return
     try {
-        if (m.text == '5') res = await fetch(`https://api.lolhuman.xyz/api/akinator/back?apikey=${global.lolkey}&server=${server}&session=${session}&signature=${signature}&step=${step}`)
-        else res = await fetch(`https://api.lolhuman.xyz/api/akinator/answer?apikey=${global.lolkey}&server=${server}&frontaddr=${frontaddr}&session=${session}&signature=${signature}&step=${step}&answer=${m.text}`)
-        anu = await res.json()
-        if (anu.status != '200') {
-            aki.sesi = false
-            aki.soal = null
-            return m.reply('Akinator session expired.')
-        }
-        anu = anu.result
-        if (anu.name) {
-            await this.sendMessage(m.chat, {
-                image: {
-                    url: anu.image
-                },
-                caption: `🎮 *Akinator Answer* 🎮\n\nDia adalah *${anu.name}*\n_${anu.description}_`,
+        if (!somematch(['0', '1', '2', '3', '4', '5'], m.text)) return this.sendMessage(m.chat, {
+            text: `[!] Jawab dengan angka 1, 2, 3, 4, atau 5\n\n${teks}`
+        }, {
+            quoted: this.akinator[m.sender].msg
+        })
+        if (this.akinator[m.sender].currentStep == '0' && m.text == '5') return m.reply('Anda telah mencapai pertanyaan pertama')
+        if (m.text == '5') {
+            await this.akinator[m.sender].back();
+            let soal = await this.sendMessage(m.chat, {
+                text: `🎮 *Akinator Back* 🎮\n_step ${this.akinator[m.sender].currentStep} ( ${this.akinator[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinator[m.sender].question}\n\n${teks}`,
                 mentions: [m.sender]
             }, {
                 quoted: m
             })
-            aki.sesi = false
-            aki.soal = null
+            this.akinator[m.sender].msg = soal
+            this.akinator[m.sender].currentStep = this.akinator[m.sender].currentStep
+            this.akinator[m.sender].progress = this.akinator[m.sender].progress
         } else {
-            soal = await this.sendMessage(m.chat, {
-                text: `🎮 *Akinator* 🎮\n_step ${anu.step} ( ${anu.progression.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${anu.question}\n\n${teks}`,
-                mentions: [m.sender]
-            }, {
-                quoted: m
-            })
-            aki.soal = soal
-            aki.step = anu.step
-            aki.progression = anu.progression
+            await this.akinator[m.sender].step(m.text);
+            if (this.akinator[m.sender].progress >= 70 || this.akinator[m.sender].currentStep >= 78) {
+                await this.akinator[m.sender].win();
+                const anu = this.akinator[m.sender].answers[0];
+                await this.sendMessage(m.chat, {
+                    image: {
+                        url: anu.absolute_picture_path
+                    },
+                    caption: `🎮 *Akinator Answer* 🎮\n\nDia adalah *${anu.name}*\n_${anu.description}_`,
+                    mentions: [m.sender]
+                }, {
+                    quoted: m
+                })
+                delete this.akinator[m.sender]
+            } else {
+                let soal = await this.sendMessage(m.chat, {
+                    text: `🎮 *Akinator* 🎮\n_step ${this.akinator[m.sender].currentStep} ( ${this.akinator[m.sender].progress.toFixed(2)} % )_\n\n@${m.sender.split('@')[0]}\n    ${this.akinator[m.sender].question}\n\n${teks}`,
+                    mentions: [m.sender]
+                }, {
+                    quoted: m
+                })
+                this.akinator[m.sender].msg = soal
+                this.akinator[m.sender].currentStep = this.akinator[m.sender].currentStep
+                this.akinator[m.sender].progress = this.akinator[m.sender].progress
+            }
         }
     } catch (e) {
-        aki.sesi = false
-        aki.soal = null
-        m.reply('Fitur Error!')
+        await m.reply(eror)
     }
     return !0
 }
