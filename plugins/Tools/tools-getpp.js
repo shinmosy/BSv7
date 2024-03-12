f3010b6da7319a386c0d219a9119a08b4ca3dadc
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-03-12 20:26:07 +0800

    Add files via upload

--- plugins/Tools/tools-getpp.js
+++ plugins/Tools/tools-getpp.js
@@ -1,14 +1,18 @@
-import fetch from 'node-fetch'
-let handler = async (m) => {
-    let who
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.sender
-    else who = m.sender
-    let url = await conn.profilePictureUrl(who, 'image')
-    await conn.sendFile(m.chat, url, 'profile.jpg', `@${who.split`@`[0]}`, m, null, {
-        mentions: [who]
-    })
-}
-handler.command = /^(get(pp|profile))$/i
-handler.help = ['getprofile [@users]']
-handler.tags = ['tools']
-export default handler
+const handler = async (m, { usedPrefix, command }) => {
+    try {
+        const who = m.quoted?.sender || m.quoted?.key?.remoteJid || m.quoted?.vM?.key?.remoteJid || m.mentionedJid?.[0] || m.sender || m.key.remoteJid || m.chat;
+        const url = await conn.profilePictureUrl(who, 'image');
+        await conn.sendFile(m.chat, url, 'profile.jpg', `@${who.split('@')[0]}`, m, null, {
+            mentions: [who]
+        });
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+handler.command = /^(get(pp|profile))$/i;
+handler.help = ['getprofile [@users]'];
+handler.tags = ['tools'];
+handler.group = true;
+
+export default handler;
--- plugins/Tools/tools-whois.js
+++ plugins/Tools/tools-whois.js
@@ -1,23 +1,28 @@
-const { v4: isIPv4, v6: isIPv6 } = await(await import('ip-regex')).default;
+import { default as ipRegex } from 'ip-regex';
 import fetch from 'node-fetch';
 
-const handler = async (m, {
-    text
-}) => {
-    if (!text) return m.reply('Input IP address format.');
-    let IP = text;
-    if (!isValidIP(IP)) return m.reply('Invalid IP address format.');
-    let ipUrls = [
-        `http://ip-api.com/json/${IP}?lang=id-ID`,
-        `http://ipwho.is/${IP}?lang=id-ID`,
-        `https://api.ipdata.co/${IP}?api-key=c6d4d04d5f11f2cd0839ee03c47c58621d74e361c945b5c1b4f668f3`,
-        `https://ipinfo.io/${IP}/json?token=41c48b54f6d78f`,
-        `https://api.ipgeolocation.io/ipgeo?apiKey=105fc2c7e8864ec08b98e1ad4e8cbc6d&ip=${IP}`,
-        `https://ipapi.co/${IP}/json`,
-        `https://sp1.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=${IP}&resource_id=5809`
-    ];
-    const result = await getData(ipUrls);
-    m.reply(JSON.stringify(result, null, 4));
+const handler = async (m, { text }) => {
+    try {
+        if (!text) throw new Error('Input IP address format.');
+        const IP = text;
+        if (!isValidIP(IP)) throw new Error('Invalid IP address format.');
+        
+        const ipUrls = [
+            `http://ip-api.com/json/${IP}?lang=id-ID`,
+            `http://ipwho.is/${IP}?lang=id-ID`,
+            `https://api.ipdata.co/${IP}?api-key=c6d4d04d5f11f2cd0839ee03c47c58621d74e361c945b5c1b4f668f3`,
+            `https://ipinfo.io/${IP}/json?token=41c48b54f6d78f`,
+            `https://api.ipgeolocation.io/ipgeo?apiKey=105fc2c7e8864ec08b98e1ad4e8cbc6d&ip=${IP}`,
+            `https://ipapi.co/${IP}/json`,
+            `https://sp1.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query=${IP}&resource_id=5809`
+        ];
+        
+        const result = await getData(ipUrls);
+        m.reply(JSON.stringify(result, null, 4));
+    } catch (error) {
+        console.error('Error:', error.message);
+        m.reply('An error occurred while processing your request.');
+    }
 };
 
 handler.help = ['whois'];
@@ -25,30 +30,26 @@ handler.tags = ['tools'];
 handler.command = /^(whois)$/i;
 export default handler;
 
-const isValidIP = (ip) => {
-    return isIPv4(ip) || isIPv6(ip);
-};
+const isValidIP = (ip) => ipRegex({ exact: true }).test(ip);
 
-const Lanang = async (url) => {
+const fetchData = async (url) => {
     try {
         const response = await fetch(url);
         if (!response.ok) throw new Error('Network response was not ok.');
         return await response.json();
     } catch (error) {
-        console.error('There was a problem with your fetch operation:', error.message);
+        console.error('Fetch operation error:', error.message);
         return null;
     }
 };
 
 const getData = async (urls) => {
     try {
-        const promises = urls.map(url => Lanang(url));
-        const results = await Promise.allSettled(promises);
-        const data = results.filter(result => result.status === "fulfilled").map(result => result.value);
-        const combinedData = data.reduce((acc, curr) => ({ ...acc, ...curr }), {});
-        return combinedData;
+        const results = await Promise.all(urls.map(url => fetchData(url)));
+        const validResults = results.filter(result => result !== null);
+        return Object.assign({}, ...validResults);
     } catch (error) {
-        console.error('There was a problem combining data:', error.message);
+        console.error('Error combining data:', error.message);
         return null;
     }
 };
