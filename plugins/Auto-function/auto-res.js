c5ee298d5d3807f9b1dcebf3f7a537a2588ad62d
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-03-26 14:28:59 +0800

    Add files via upload

--- plugins/Auto-function/auto-res.js
+++ plugins/Auto-function/auto-res.js
@@ -1,40 +1,19 @@
-import fetch from 'node-fetch'
-import fs from 'fs'
+import fetch from 'node-fetch';
+import fs from 'fs';
 
 export async function all(m) {
-    let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png')
+    if (!this || m.isBaileys || m.chat.endsWith('broadcast') || !m.isGroup || !m.sender || !m.mentionedJid.includes(this.user.jid)) return;
 
-    let vr = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-    let num = vr.getRandom()
-    let stc = await fs.readFileSync('./sticker/ress' + num + '.webp')
-
-
-    if (m.isBaileys) return
-    if (m.chat.endsWith('broadcast')) return
     try {
-        if (m.mentionedJid.includes(this.user.jid) && m.isGroup) {
-            await this.sendMessage(m.chat, {
-                sticker: stc,
-                thumbnail: await (await fetch(pp)).arrayBuffer(),
-                contextInfo: {
-                    externalAdReply: {
-                        showAdAttribution: true,
-                        mediaType: 1,
-                        mediaUrl: sig,
-                        title: '「 ❔ 」',
-                        body: wm,
-                        sourceUrl: 'http://github.com/AyGemuy',
-                        thumbnail: await (await fetch(pp)).arrayBuffer()
-                    }
-                }
-            }, {
-                quoted: m
-            })
+        let pp = await this.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/24fa902ead26340f3df2c.png');
+        let stc = await fs.promises.readFile(`./sticker/ress${pickRandom(1, 9)}.webp`);
 
-        }
+        await this.sendMessage(m.chat, {sticker: stc, thumbnail: await (await fetch(pp)).arrayBuffer(), contextInfo: {externalAdReply: {showAdAttribution: true, mediaType: 1, mediaUrl: sig, title: '「 ❔ 」', body: wm, sourceUrl: 'http://github.com/AyGemuy', thumbnail: await (await fetch(pp)).arrayBuffer(),},},}, {quoted: m,});
     } catch (e) {
-        return
+        console.error(e);
     }
+}
 
-
+function pickRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
--- plugins/Auto-function/auto-skak.js
+++ plugins/Auto-function/auto-skak.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 export async function all(m) {
-    if (m.isBaileys || !m.text) return
-    let MengSkak = /^(napa|halah|cih|cuih|yaha|erorr|kasian|dek|gajelas|bokep)$/i.test(m.text)
+    if (!this || m.isBaileys || !m.text) return;
+
+    const MengSkak = /^(napa|halah|cih|cuih|yaha|erorr|kasian|dek|gajelas|bokep)$/i.test(m.text);
     if (MengSkak && m.isGroup) {
         const options = {
             method: 'POST',
@@ -13,29 +14,26 @@ export async function all(m) {
                 'X-RapidAPI-Host': 'openai37.p.rapidapi.com'
             },
             data: {
-                messages: [{
-                        role: 'assistant',
-                        content: 'Hello there! How may I assist you today?'
-                    },
-                    {
-                        role: 'user',
-                        content: m.text
-                    }
+                messages: [
+                    { role: 'assistant', content: 'Hello there! How may I assist you today?' },
+                    { role: 'user', content: m.text }
                 ],
                 temperature: 1
             }
         };
+
         try {
             const response = await axios(options);
             await this.reply(m.chat, response.data.content, m, adReply);
         } catch (error) {
             console.error(error);
+
             try {
                 const kanyeResponse = await axios.get('https://api.kanye.rest/');
                 await this.reply(m.chat, kanyeResponse.data.quote, m, adReply);
             } catch (kanyeError) {
                 console.error(kanyeError);
-                return null; // Atau lakukan sesuatu dengan error ini sesuai kebutuhan Anda.
+                return null;
             }
         }
     }
