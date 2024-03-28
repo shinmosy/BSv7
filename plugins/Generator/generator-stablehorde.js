e46588c51f2e4ffecedcd50b69b7c59f7d5a49b5
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2024-03-28 13:19:01 +0800

    Add files via upload

--- plugins/Generator/generator-stablehorde.js
+++ plugins/Generator/generator-stablehorde.js
@@ -1,16 +1,9 @@
-const {
-    StableHorde
-} = await(await import('../../lib/maker/stablehorde.js'));
+import { StableHorde } from '../../lib/maker/stablehorde.js';
+
 const apiKey = "0000000000";
 const stableHorde = new StableHorde({ apiKey });
 
-let handler = async (m, {
-    command,
-    usedPrefix,
-    conn,
-    text,
-    args
-}) => {
+let handler = async (m, { command, usedPrefix, conn, text, args }) => {
     const input_data = ["text", "image"];
     let [urutan, tema, queer] = text.split("|");
     urutan = parseInt(urutan);
@@ -22,35 +15,40 @@ let handler = async (m, {
     }
 
     let out = input_data[urutan - 1];
-    if (out == "text") {
-        const textModels = await stableHorde.textModels();
-        if (isNaN(tema) || tema < 1 || tema > textModels.length) {
-            const validTemaList = textModels.map((item, index) => `${index + 1}. ${item}`).join('\n');
-            return m.reply(`Nomor yang Anda masukkan tidak valid. Harap pilih nomor antara 1 dan ${textModels.length}:\n${validTemaList}`);
-        }
-        const model = textModels[tema - 1];
-        const prompt = queer;
-        try {
-            const generatedText = await stableHorde.generateText(prompt, model);
-            await m.reply(generatedText);
-        } catch (e) {
-            return m.reply("Terjadi kesalahan saat menghasilkan teks.");
-        }
-    } else if (out == "image") {
-        const imageModels = await stableHorde.imageModels();
-        const sortedNames = imageModels.sort((a, b) => b.performance - a.performance).map(item => item.name);
-        if (isNaN(tema) || tema < 1 || tema > sortedNames.length) {
-            const validTemaList = sortedNames.map((item, index) => `${index + 1}. ${item}`).join('\n');
-            return m.reply(`Nomor yang Anda masukkan tidak valid. Harap pilih nomor antara 1 dan ${sortedNames.length}:\n${validTemaList}`);
-        }
-        const model = sortedNames[tema - 1];
-        const prompt = queer;
-        try {
-            const generatedImageURL = await stableHorde.generateImage(model, prompt);
-            await conn.sendFile(m.chat, generatedImageURL, '', '', m);
-        } catch (e) {
-            return m.reply("Terjadi kesalahan saat menghasilkan gambar.");
-        }
+    if (!queer) return m.reply("Harap masukkan query untuk menghasilkan teks atau gambar.");
+
+    switch (out) {
+        case "text":
+            const textModels = await stableHorde.textModels();
+            if (isNaN(tema) || tema < 1 || tema > textModels.length) {
+                const validTemaList = textModels.map((item, index) => `${index + 1}. ${item}`).join('\n');
+                return m.reply(`Nomor yang Anda masukkan tidak valid. Harap pilih nomor antara 1 dan ${textModels.length}:\n${validTemaList}`);
+            }
+            const modelText = textModels[tema - 1];
+            try {
+                const generatedText = await stableHorde.generateText(queer, modelText);
+                await m.reply(generatedText);
+            } catch (e) {
+                return m.reply("Terjadi kesalahan saat menghasilkan teks.");
+            }
+            break;
+        case "image":
+            const imageModels = await stableHorde.imageModels();
+            const sortedNames = imageModels.sort((a, b) => b.performance - a.performance).map(item => item.name);
+            if (isNaN(tema) || tema < 1 || tema > sortedNames.length) {
+                const validTemaList = sortedNames.map((item, index) => `${index + 1}. ${item}`).join('\n');
+                return m.reply(`Nomor yang Anda masukkan tidak valid. Harap pilih nomor antara 1 dan ${sortedNames.length}:\n${validTemaList}`);
+            }
+            const modelImage = sortedNames[tema - 1];
+            try {
+                const generatedImageURL = await stableHorde.generateImage(modelImage, queer);
+                await conn.sendFile(m.chat, generatedImageURL, '', '', m);
+            } catch (e) {
+                return m.reply("Terjadi kesalahan saat menghasilkan gambar.");
+            }
+            break;
+        default:
+            return m.reply("Jenis input yang diberikan tidak valid.");
     }
 }
 
